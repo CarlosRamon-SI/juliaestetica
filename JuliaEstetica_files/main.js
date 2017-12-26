0602195e38e8b20403f5ca72b8c7111567643f59
Carlos Ramon Moreira Leite <ramon.ethan@gmail.com>
2017-12-26 17:58:20 -0300

    Update main.js

--- JuliaEstetica_files/main.js
+++ JuliaEstetica_files/main.js
@@ -63,7 +63,7 @@ $(document).ready(function () {
 
 $(document).ready(function () {
     var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg', '21.jpg', '22.jpg', '23.jpg', '24.jpg', '25.jpg', '26.jpg', '27.jpg'];
-    $('.dynamic-banner').css({ 'background': 'no-repeat center top url(./JuliaEstetica_files2/img/banners/' + images[Math.floor(Math.random() * images.length)] + ')' });
+    $('.dynamic-banner').css({ 'background': 'no-repeat center top url(./JuliaEstetica_files/img/banners/' + images[Math.floor(Math.random() * images.length)] + ')' });
 });
 
 $(window).scroll(function () {
